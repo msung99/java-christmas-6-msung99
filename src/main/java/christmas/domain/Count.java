919151fb20cd48bb33bd65fccf636e74465297aa
msung99 <msung6924@naver.com>
2023-11-10 18:49:13 +0900

    feat: 메뉴 개수가 1 미만인 값을 입력한 경우 예외가 발생하는 기능 구현

--- src/main/java/christmas/domain/Count.java
+++ src/main/java/christmas/domain/Count.java
@@ -1,11 +1,14 @@
 package christmas.domain;
 
 import christmas.exception.NumberParseException;
+import christmas.exception.OrderItemParseException;
 
 public class Count {
     private int count;
+    private static final int MIN_COUNT = 1;
 
     private Count(int count){
+        validateOutOfRange(count);
         this.count = count;
     }
 
@@ -18,6 +21,12 @@ public class Count {
             return Integer.parseInt(count);
         } catch (NumberFormatException e) {
             throw new NumberParseException();
+        }
+    }
+
+    private static void validateOutOfRange(int count){
+        if(count < MIN_COUNT) {
+            throw new OrderItemParseException();
         }
     }
 }
