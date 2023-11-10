da6b69579826253ee5dc26451f1a67a49606ab6c
msung99 <msung6924@naver.com>
2023-11-10 18:40:58 +0900

    test: 공백을 입력 중간에 포함하여 주문 메뉴과 개수를 입력하는 경우 예외가 발생하는지 테스트, 추가 테스트케이스 삽입

--- src/test/java/christmas/domain/OrderParserTest.java
+++ src/test/java/christmas/domain/OrderParserTest.java
@@ -8,8 +8,9 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class OrderParserTest {
     @ParameterizedTest
-    @DisplayName("구분자 - 를 생략하는 경우 예외가 발생한다.")
-    @ValueSource(strings = {"티본스테이크1,바비큐립-1,초코케이크-2,제로콜라-1", "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라1"})
+    @DisplayName("구분자 , 를 생략하는 경우 예외가 발생한다.")
+    @ValueSource(strings = {"티본스테이크-1바비큐립-1,초코케이크-2,제로콜라-1", "티본스테이크-1,바비큐립-1,초코케이크-2제로콜라-1",
+            "티본스테이크-1,바비큐립-1,초코케이크-2제로콜라-1,", ",티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1"})
     void testOrderParseSkipUnitDelimiter(String input){
         Assertions.assertThatThrownBy(() -> OrderParser.parseOrderUnits(input))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -17,8 +18,17 @@ public class OrderParserTest {
 
     @ParameterizedTest
     @DisplayName("구분자 - 를 생략하는 경우 예외가 발생한다.")
-    @ValueSource(strings = {"티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1"})
+    @ValueSource(strings = {"티본스테이크-1,바비큐립1,초코케이크-2,제로콜라1", "티본스테이크1,바비큐립-1,초코케이크-2,제로콜라-1",
+    "-티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1"})
     void testOrderParseSkipUnitDelimite2r(String input){
+        Assertions.assertThatThrownBy(() -> OrderParser.parseOrderUnits(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("공백을 입력 중간에 포함하는 경우 예외가 발생한다.")
+    @ValueSource(strings = {"티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1 ", "티본스테이크-1, 바비큐립-1,초코케이크  -2,제로콜라-1"})
+    void testOrderParseWithEmptySpace(String input){
         Assertions.assertThatThrownBy(() -> OrderParser.parseOrderUnits(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
