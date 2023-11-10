ef9394f9547bfef0ee16cfbc5c1221e72670eb67
msung99 <msung6924@naver.com>
2023-11-10 11:44:36 +0900

    feat: InputView, OutputView 클래스 생성

--- src/main/java/christmas/EventPlanController.java
+++ src/main/java/christmas/EventPlanController.java
@@ -2,6 +2,11 @@ package christmas;
 
 public class EventPlanController {
     public void runPlanner(){
+        initDatas();
 
+    }
+
+    private void initDatas(){
+        // TODO: 기본 데이터 초기화
     }
 }
