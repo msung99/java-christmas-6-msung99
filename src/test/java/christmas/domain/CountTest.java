9c12b8094cafcd9a4ad9036a8cc12328ac508c7b
msung99 <msung6924@naver.com>
2023-11-10 18:49:34 +0900

    test: 메뉴의 개수를 1미만으로 입력하는 경우 예외가 발생하는지 테스트

--- src/test/java/christmas/domain/CountTest.java
+++ src/test/java/christmas/domain/CountTest.java
@@ -1,5 +1,17 @@
 package christmas.domain;
 
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 public class CountTest {
+    @ParameterizedTest
+    @DisplayName("메뉴의 개수를 1미만으로 입력하는 경우 예외가 발생한다.")
+    @ValueSource(strings = {"0", "-1"})
+    void testCountOutOfRange(String count){
+        Assertions.assertThatThrownBy(() -> Count.of(count))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
