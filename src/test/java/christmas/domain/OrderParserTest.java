182f0bc4e25cd7aa9cc7dff9f81fe0a19e714f8c
msung99 <msung6924@naver.com>
2023-11-10 18:17:14 +0900

    test: 구분자 , 를 생략하는 경우 예외가 발생하는지 테스트

--- src/test/java/christmas/domain/OrderParserTest.java
+++ src/test/java/christmas/domain/OrderParserTest.java
@@ -14,4 +14,12 @@ public class OrderParserTest {
         Assertions.assertThatThrownBy(() -> OrderParser.parseOrderUnits(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @ParameterizedTest
+    @DisplayName("구분자 , 를 생략하는 경우 예외가 발생한다.")
+    @ValueSource(strings = {"티본스테이크-1바비큐립-1,초코케이크-2,제로콜라-1", "티본스테이크-1,바비큐립-1,초코케이크-2제로콜라1"})
+    void testOrderParseSkipItemDelimiter(String input){
+        Assertions.assertThatThrownBy(() -> OrderParser.parseOrderUnits(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
