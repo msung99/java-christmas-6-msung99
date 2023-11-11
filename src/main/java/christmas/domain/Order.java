17db39a8dc4c8176659c08fb1b9a5e9be2480668
msung99 <msung6924@naver.com>
2023-11-11 14:09:23 +0900

    feat: 중복 메뉴를 입력한 경우 예외가 발생하는 기능 구현

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,19 +1,25 @@
 package christmas.domain;
 
 import christmas.domain.menu.Menu;
+import christmas.exception.InvalidOrderException;
 import christmas.exception.MaxOrderSizeException;
+import christmas.repository.MenuRepository;
 import christmas.util.OrderParser;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Order {
     private final Map<Menu, Quantity> order;
     private static final int MAX_ORDER_SIZE = 20;
+    private static final MenuRepository memberRepository = MenuRepository.getInstance();
 
     private Order(Map<Menu, Quantity> order){
         validateOverSize(order);
         validateOnlyBeverage();
-        validateDuplicateMenu();
+        validateDuplicateMenu(order);
         this.order = new HashMap<>(order);
     }
 
@@ -29,13 +35,14 @@ public class Order {
         }
     }
 
-
-
     private void validateOnlyBeverage(){
         // TODO: 음료만 주문했는지 검증
     }
 
-    private void validateDuplicateMenu(){
-        // TODO: 중복 메뉴가 발생했는지 검증
+    private void validateDuplicateMenu(Map<Menu, Quantity> order) {
+        if (order.keySet().size() != order.keySet().stream().distinct().count()) {
+            throw new InvalidOrderException();
+        }
     }
+
 }
