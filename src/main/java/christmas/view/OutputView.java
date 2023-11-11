4b452c3e1df36a23310c0aec84c34140f806224f
msung99 <msung6924@naver.com>
2023-11-11 15:54:34 +0900

    refactor: 출력 문구 상수화

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -4,12 +4,14 @@ import christmas.domain.Order;
 import christmas.domain.Quantity;
 import christmas.domain.menu.Menu;
 import java.util.Map;
-import org.mockito.internal.matchers.Or;
 
 public class OutputView {
+    private static final String DAY_EVENT_INTRO_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
+    private static final String ORDER_MENU_OUTLINE_MESSAGE = "<주문 메뉴>";
+
     public void printMenu(Order order) {
-        System.out.println("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n");
-        System.out.println("<주문 메뉴>");
+        System.out.println(DAY_EVENT_INTRO_MESSAGE);
+        System.out.println(ORDER_MENU_OUTLINE_MESSAGE);
         for(Map.Entry<Menu, Quantity> orderUnit : order.getOrder().entrySet()) {
             Menu menu = orderUnit.getKey();
             Quantity quantity = orderUnit.getValue();
