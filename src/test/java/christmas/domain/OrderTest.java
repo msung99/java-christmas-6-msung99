408dd217a3b9196e4e730884d3fe4c31217a024f
msung99 <msung6924@naver.com>
2023-11-11 13:17:54 +0900

    test: 주문 메뉴 개수가 20개를 초과하는 경우 예외가 발생하는지 테스트

--- src/test/java/christmas/domain/OrderTest.java
+++ src/test/java/christmas/domain/OrderTest.java
@@ -9,7 +9,8 @@ public class OrderTest {
 
     @ParameterizedTest
     @DisplayName("주문 개수가 20개를 초과하는 경우 예외가 발생한다.")
-    @ValueSource(strings = {"티본스테이크-1,바비큐립-1,초코케이크-1", "티본스테이크-1,바비큐립-1,초코케이크-1"})
+    @ValueSource(strings = {"티본스테이크-20,바비큐립-1,초코케이크-1", "티본스테이크-5,바비큐립-30,초코케이크-3",
+            "티본스테이크-1,바비큐립-1,초코케이크-19"})
     public void testCountOutOfRange(String order){
         Assertions.assertThatThrownBy(() -> Order.of(order))
                 .isInstanceOf(IllegalArgumentException.class);
