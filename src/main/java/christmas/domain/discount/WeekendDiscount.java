69674a5b67e6e521f38844d1ae8562f5833e42f6
msung99 <msung6924@naver.com>
2023-11-12 18:40:12 +0900

    feat: 주말 할인, 메인 메뉴 1개당 2,023원을 할인하는 기능 구현

--- src/main/java/christmas/domain/discount/WeekendDiscount.java
+++ src/main/java/christmas/domain/discount/WeekendDiscount.java
@@ -3,17 +3,28 @@ package christmas.domain.discount;
 import christmas.domain.Date;
 import christmas.domain.DateType;
 import christmas.domain.EventProgressStatus;
+import christmas.domain.Order;
+import christmas.domain.menu.MenuType;
 import christmas.repository.DateRepository;
+import christmas.repository.MenuRepository;
 import java.util.List;
 
 public class WeekendDiscount implements Discount{
     private List<Date> dates = initDays();
     private static final int DISCOUNT_PRICE = 2023;
     private static final DateRepository dateRepository = DateRepository.getInstance();
+    private static final MenuRepository menuRepository = MenuRepository.getInstance();
 
     @Override
     public int getDiscountPrice(EventProgressStatus eventProgressStatus) {
-        return DISCOUNT_PRICE;
+        return DISCOUNT_PRICE * getMainCounts(eventProgressStatus.getOrder());
+    }
+
+    private int getMainCounts(Order order) {
+        return order.getOrder().entrySet().stream()
+                .filter(entry -> menuRepository.findTypeByMenu(entry.getKey()) == MenuType.MAIN)
+                .mapToInt(entry -> entry.getValue().getQuantity())
+                .sum();
     }
 
     @Override
