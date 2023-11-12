56e4fdb02ccba0f6ae8a00c7c160f28c1d158030
msung99 <msung6924@naver.com>
2023-11-12 20:32:08 +0900

    test: 주말 날짜에 해당하는 경우 주말 할인이 적용되는지 테스트

--- src/test/java/christmas/domain/WeekendDiscountTest.java
+++ src/test/java/christmas/domain/WeekendDiscountTest.java
@@ -1,5 +1,6 @@
 package christmas.domain;
 
+import christmas.domain.discount.Discount;
 import christmas.domain.discount.WeekdayDiscount;
 import christmas.domain.discount.WeekendDiscount;
 import java.util.stream.Stream;
@@ -8,8 +9,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class WeekendDiscountTest {
+    @ParameterizedTest
+    @DisplayName("주말 날짜에 해당하는 경우 주말 할인이 적용된다.")
+    @ValueSource(strings = {"1", "2", "8", "9", "15", "16", "22", "23", "29", "30"})
+    void testSpecialDiscountApply(String date) {
+        Discount weekdayDiscount = new WeekendDiscount();
+        Assertions.assertTrue(weekdayDiscount.isEventDay(Date.of(date)));
+    }
+
     @ParameterizedTest
     @DisplayName("주말 할인을 적용하는 경우 메인 메뉴 개수를 카운팅하여 적용한다")
     @MethodSource("getWeekendOrderInputs")
