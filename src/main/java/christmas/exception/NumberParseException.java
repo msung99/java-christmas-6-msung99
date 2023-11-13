2078f865d6ffcebbfe9385effe2ac142a542f922
msung99 <msung6924@naver.com>
2023-11-13 14:13:16 +0900

    style: 불필요한 예외처리 클래스 제거

--- src/main/java/christmas/exception/NumberParseException.java
+++
@@ -1,9 +0,0 @@
-package christmas.exception;
-
-public class NumberParseException extends CustomException {
-    private static final String ERROR_MESSAGE = "숫자로만 입력해주세요.";
-
-    public NumberParseException() {
-        super(ERROR_MESSAGE);
-    }
-}
