e87e4a8f8f84bef08faf4821a4d8f100cafef23f
msung99 <msung6924@naver.com>
2023-11-13 14:00:46 +0900

    test: 총혜택 금액이 10000~19999원 사이인 경우 트리를 부여받는지 테스트

--- src/test/java/christmas/domain/BadgeTest.java
+++ src/test/java/christmas/domain/BadgeTest.java
@@ -10,7 +10,14 @@ public class BadgeTest {
     @ParameterizedTest
     @DisplayName("총혜택 금액이 5000~9999원 사이인 경우 별을 부여받는다.")
     @ValueSource(ints = {5000, 6000, 9000, 9999})
-    void findBadgeTest(int discountPrice) {
+    void findStarBadgeTest(int discountPrice) {
         Assertions.assertEquals(Badge.findByValue(discountPrice), Badge.STAR);
+    }
+
+    @ParameterizedTest
+    @DisplayName("총혜택 금액이 10000~19999원 사이인 경우 트리를 부여받는다.")
+    @ValueSource(ints = {10000, 15000, 19000, 19999})
+    void findTreeBadgeTest(int discountPrice) {
+        Assertions.assertEquals(Badge.findByValue(discountPrice), Badge.TREE);
     }
 }
