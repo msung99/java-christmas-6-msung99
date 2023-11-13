2676968fa57bcbd46a2378059a0d10bdc59987e4
msung99 <msung6924@naver.com>
2023-11-13 14:02:39 +0900

    test: 총혜택 금액이 20000원 이상인 경우 산타 배지를 부여받는지 테스트

--- src/test/java/christmas/domain/BadgeTest.java
+++ src/test/java/christmas/domain/BadgeTest.java
@@ -8,16 +8,23 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class BadgeTest {
     @ParameterizedTest
-    @DisplayName("총혜택 금액이 5000~9999원 사이인 경우 별을 부여받는다.")
+    @DisplayName("총혜택 금액이 5000~9999원 사이인 경우 별 배지을 부여받는다.")
     @ValueSource(ints = {5000, 6000, 9000, 9999})
     void findStarBadgeTest(int discountPrice) {
         Assertions.assertEquals(Badge.findByValue(discountPrice), Badge.STAR);
     }
 
     @ParameterizedTest
-    @DisplayName("총혜택 금액이 10000~19999원 사이인 경우 트리를 부여받는다.")
+    @DisplayName("총혜택 금액이 10000~19999원 사이인 경우 트리 배지를 부여받는다.")
     @ValueSource(ints = {10000, 15000, 19000, 19999})
     void findTreeBadgeTest(int discountPrice) {
         Assertions.assertEquals(Badge.findByValue(discountPrice), Badge.TREE);
+    }
+
+    @ParameterizedTest
+    @DisplayName("총혜택 금액이 20000원 이상인 경우 산타 배지를 부여받는다.")
+    @ValueSource(ints = {20000, 109000, 500200, 9999999})
+    void findSantaBadgeTest(int discountPrice) {
+        Assertions.assertEquals(Badge.findByValue(discountPrice), Badge.SANTA);
     }
 }
